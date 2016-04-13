f873aebf0d51918f384cd94c486756d1e886c9ce
Jack Kavanagh <jackkav@gmail.com>
2016-04-14 00:17:29 +0800

    added login logic

--- client/modules/core/components/MainLayout.jsx
+++ client/modules/core/components/MainLayout.jsx
@@ -4,8 +4,17 @@ const Layout = ({ content }) => (
   <Grid>
     <Row>
       <Col xs={12}>
-        <a href="/login">Login</a> | <a href="/register">Register</a>
-        <PageHeader>{Meteor.userId()?'Welcome '+Meteor.user().username+ ' what you waiting for vote for something' :'Vote for pedro'} </PageHeader>
+        {
+          Meteor.userId() ?
+          <Row>Welcome {Meteor.user().username} <a href="/logout">Logout</a> </Row>
+          :
+          <Row><a href="/login">Login</a> | <a href="/register">Register</a></Row>
+        }
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={12}>
+        <PageHeader>{Meteor.userId() ? 'What you waiting for vote for something' : 'Vote for pedro'} </PageHeader>
       </Col>
     </Row>
      {content()}
