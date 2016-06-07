1588f27d853e56e80a7cd2fa3506ced9d7652f1e
Jack Kavanagh <jackkav@gmail.com>
2016-06-07 20:06:44 +0800

    updated gitignores for new structure update, and added mobile config example

---
+++ project/mobile-config.js
@@ -0,0 +1,19 @@
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  id: 'com.ihealthlabs.ijiankang.patient.ios',
+  // id: 'com.ihealthlabs.bp.mantra.demo.ios',
+  name: 'iHealth',
+  description: 'iHealth BP Mantra Demo',
+  author: 'iHealthLabs',
+  website: 'http://www.ihealthlabs.com.cn',
+  version: '0.0.1',
+  buildNumber: '1'
+});
+
+App.accessRule('*');
+// App.accessRule("blob:*");
+App.setPreference('StatusBarOverlaysWebView', 'false');
+App.setPreference('StatusBarBackgroundColor', '#008CE0');
+App.setPreference('orientation', 'portrait');
+App.setPreference('SplashScreenDelay', 15);
